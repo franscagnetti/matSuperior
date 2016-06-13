7e6ef7dc0ba0e161fe43217730d21dfecd0b8b02
franscagnetti <franscagnetti@hotmail.com>
2016-06-12 23:20:23 -0300

    agrego .wav e inicio del script de matlab -leo audio y veo el grfico

---
+++ matSuperior.m
@@ -0,0 +1,11 @@
+load handel.mat
+
+[y,Fs] = audioread('Audio-TPMatSup.wav');
+
+
+t = 0:seconds(1/Fs):seconds(info.Duration);
+t = t(1:end-1);
+
+plot(t,y);
+xlabel('Time');
+ylabel('Audio Signal');

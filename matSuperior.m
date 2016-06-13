bb66f4baff65ec4fe413753ba7321deffac90ba8
franscagnetti <franscagnetti@hotmail.com>
2016-06-13 11:47:05 -0300

    arreglo clase info

--- matSuperior.m
+++ matSuperior.m
@@ -1,11 +1,11 @@
-load handel.mat
+load handel.mat;
 
 [y,Fs] = audioread('Audio-TPMatSup.wav');
 
+info = audioinfo('Audio-TPMatSup.wav')
 
 t = 0:seconds(1/Fs):seconds(info.Duration);
 t = t(1:end-1);
-
 plot(t,y);
 xlabel('Time');
 ylabel('Audio Signal');
